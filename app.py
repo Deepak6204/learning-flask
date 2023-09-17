ac69b02aa932a7a4bcb8804f05bb0d72ce862fe0
Deepak6204 <127977680+Deepak6204@users.noreply.github.com>
2023-09-17 14:15:37 +0530

    added dynamix data

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
+Jobs = [{
+    'id':1,
+    'title':'Data Analyst',
+    'location': 'Bengaluru',
+    'salary': 'rs 10,00,000'
+},
+{
+    'id':2,
+    'title':'Data Scientist',
+    'location': 'Delhi',
+    'salary': 'rs 15,00,000'
+},
+{
+    'id':3,
+    'title':'Front end dev',
+    'location': 'Remote',
+},
+{
+    'id':4,
+    'title':'Backend',
+    'location': 'Remote',
+    'salary': 'rs 20,00,000'
+}
+
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hi this is Deepak Yadav</p>"
+    return render_template('home.html', jobs= Jobs)
+
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(Jobs)
+
 
 
 if(__name__ == '__main__'):
